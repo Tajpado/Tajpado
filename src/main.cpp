e107818f81bd8882bab125ae2364478b4ac60240
312k <55316963+312k@users.noreply.github.com>
2022-06-13 23:07:42 +0900

    日本語を表示できるようにした

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,55 @@
-#include <SDL.h>
+ï»¿#include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <iostream>
+#include <string>
+
 
 int main(int argc, char** argv) {
 	SDL_Window* window;
 	SDL_Event exit;
-	// SDL2‚Ì‰Šú‰»
+	// SDL2ã®åˆæœŸåŒ–
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		SDL_Quit();
 		return 1;
 	}
-	// Window‚Ìì¬ATTF‚Ì‰Šú‰»‚É¸”s‚µ‚½‚Æ‚«
+	// Windowã®ä½œæˆã€TTFã®åˆæœŸåŒ–ã«å¤±æ•—ã—ãŸã¨ã
 	if ((window = SDL_CreateWindow("Tajpado", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_RESIZABLE)) == NULL || TTF_Init() == -1) {
 		SDL_Quit();
 		return 1;
 	}
-	TTF_Font* font;
+	TTF_Font* font=NULL;
 
+	SDL_Rect time_rect{
+		time_rect.x = 520,
+		time_rect.y = 260,
+		time_rect.w = 270,
+		time_rect.h = 240
+	};
 	SDL_SetWindowTitle(window, "Tajpado");
-	//ƒŒƒ“ƒ_ƒ‰[
+	//ãƒ¬ãƒ³ãƒ€ãƒ©ãƒ¼
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	//”wŒi‚ğ•‚É‚·‚é
+	//èƒŒæ™¯ã‚’é»’ã«ã™ã‚‹
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	//”wŒi‚ğƒNƒŠƒA
+	//èƒŒæ™¯ã‚’ã‚¯ãƒªã‚¢
 	SDL_RenderClear(renderer);
-
+	//ãƒ•ã‚©ãƒ³ãƒˆã‚’èª­ã¿è¾¼ã‚€
+	if ((font = TTF_OpenFont("C:/Windows/Fonts/HackGen35Nerd-Bold.ttf", 100)) == NULL) {
+		SDL_Quit();
+		return 1;
+	}
 	while (1)
 	{
-		//•Â‚¶‚éƒ{ƒ^ƒ“‚Å•Â‚¶‚ê‚é‚æ‚¤‚É‚·‚é
+		//é–‰ã˜ã‚‹ãƒœã‚¿ãƒ³ã§é–‰ã˜ã‚Œã‚‹ã‚ˆã†ã«ã™ã‚‹
 		SDL_PollEvent(&exit);
 		if (exit.type == SDL_QUIT) {
 			break;
 		}
+		SDL_RenderCopy(renderer,
+			SDL_CreateTextureFromSurface(renderer,
+				TTF_RenderUTF8_Blended(font, reinterpret_cast<const char*>(u8"ã‚¢ã‚¤ã‚¹ã‚’æ„›ã™"), SDL_Color{0,120, 120, 120})),
+			NULL, &time_rect);
+		SDL_RenderPresent(renderer);
 	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
